932fbe21d57d8597655ab1c4eb2f2d0b761fe039
MiguelJerome <sharkmaycry@gmail.com>
2023-02-05 01:57:44 -0500

    fixing footer name class css

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     return <>
      <main>
         <div className={styles.container}>
-            <h1 className={styles.h1} >404 Page Not Found</h1>
+            <h1 className={styles.h1} >404 Page non trouvée</h1>
             <h1 className={styles.emoji} role="img" aria-label="Face With Rolling Eyes Emoji">🙄</h1>
         </div>
         <div className={styles.container}>
