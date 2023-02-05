bad4a541f76f6017b193f6e546058d1283cc5ba9
MiguelJerome <sharkmaycry@gmail.com>
2023-02-05 16:25:11 -0500

    add liste image tout magasiner page

--- components/ProduitListe.jsx
+++ components/ProduitListe.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/ProduitListe.module.css';
 export default function ProduitListe() {
     return (<>
         
-        <div className={styles.superConatainer}>
+        <div className={styles.superContainer}>
             <div className={styles.container}>
                 <ProduitItem />
             </div>
