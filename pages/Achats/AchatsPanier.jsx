12d621a165ffe486822baf38d13341d8f6c48ae3
MiguelJerome <sharkmaycry@gmail.com>
2023-02-12 13:16:09 -0500

    cleaning side panel code

--- pages/Achats/AchatsPanier.jsx
+++ pages/Achats/AchatsPanier.jsx
@@ -11,7 +11,8 @@ import Image from 'next/image';
 import FermerPanier from '/public/img/FermerPanier.svg';
 import CheckoutPanier from '/public/img/cart.png';
 import { Produitsdisponibles } from '/components/AchatPanier/Produitsdisponibles';
-import PanierVideMessage from '/components/AchatPanier/PanierVideMessage'
+import PanierVideMessage from '/components/AchatPanier/PanierVideMessage';
+import ListeItemPanier from '/components/AchatPanier/PanierPanneauDroit/ListeItemPanier';
 
 export default function AchatsPanier() {
   const [cart, initCart, setCart, removeFromCart] = useCart([]);
@@ -119,53 +120,16 @@ export default function AchatsPanier() {
                     <PanierVideMessage />
                   </>
                 ) : (
-                  <>
-                    <h3 className={styles.subTitle}>Articles dans votre panier:</h3>
-                    <ul>
-                      {cart.map((item) => (
-                        <React.Fragment key={item._id}>
-                          <li className={styles.produitDisponible}>
-                            <Image
-                              className={`${styles.imgCard} ${styles.img}`}
-                              src={item.src}
-                              alt={item.alt}
-                              width={Number(item.averageWidth) || 100}
-                              height={Number(item.averageHeight) || 100}
-                              onClick={() => router.push(`/produit/${item.name}`)}
-                            />
-                            <div>
-                              <div className={styles.cartFormWragpe}>
-                                <p className={styles.productInfo}>{item.name}</p>
-                                <p className={styles.productInfo}>${item.price}</p>
-                              </div>
-                              <span>Qty:</span>
-                              <input
-                                className={styles.input}
-                                type="number"
-                                placeholder="1"
-                                value={item.purchaseQuantity}
-                                onChange={(e) => handleChange(item, parseInt(e.target.value))}
-                              />
-                              <span role="img" aria-label="trash" onClick={() => removeFromCart(item)} className={styles.imgCard}>🗑️</span>
-                            </div>
-                          </li>
-                          <li className={styles.itemTotal}>
-                            {item.name} - {parseInt(item.purchaseQuantity, 10)} x ${parseFloat(item.price).toFixed(2)} = $
-                            {(parseInt(item.purchaseQuantity, 10) * parseFloat(item.price)).toFixed(2)}
-                          </li>
-                        </React.Fragment>
-                      ))}
-                    </ul>
-                    <div className={styles.grandTotal}>
-                      <strong>Grand Total: ${parseFloat(total).toFixed(2)}</strong>
-                    </div>
-                    <div>
-                      <strong>Total item: {calculateTotal()}</strong>
-                      <button className={styles.boutonCheckout} onClick={submitCheckout}>
-                        <Image src={CheckoutPanier} alt="Checkout Panier" className={styles.imgCheckout} />
-                      </button>
-                      <span>(log in to check out)</span>
-                    </div>
+                    <>
+                      <ListeItemPanier
+                       cart={cart}
+                       handleChange={handleChange}
+                       removeFromCart={removeFromCart}
+                       router={router}
+                       calculateTotal={calculateTotal}
+                       total={total}
+                       submitCheckout={submitCheckout}
+                     />
                   </>
                 )}
                 <Produitsdisponibles produits={produits} />
