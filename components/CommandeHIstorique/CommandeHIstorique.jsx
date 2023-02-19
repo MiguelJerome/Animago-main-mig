6de4975d8cd5979d82cd601a64563cfab126362f
MiguelJerome <sharkmaycry@gmail.com>
2023-02-19 16:26:42 -0500

    synchronizing cart with orders

--- components/CommandeHIstorique/CommandeHIstorique.jsx
+++ components/CommandeHIstorique/CommandeHIstorique.jsx
@@ -3,39 +3,45 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import styles from '/styles/Cart.module.css';
 
-export default function OrderHistory({ cart, purchaseDate, purchaseTime, currentTime, orders }) {
+export default function CommandeHIstorique({ cart, purchaseDate, purchaseTime, currentTime, orders }) {
     const router = useRouter();
     console.log(`Order history is Now: ${purchaseDate} ${currentTime}`, orders);
 
     return (
         <div className={styles.containerMainOrder}>
             <div className={styles.OrderTitle}>
-                <h2>Historique des commandes pour {purchaseDate} </h2>
-                <h2>{currentTime} </h2>
+                <h2>Historique des commandes pour {purchaseDate}</h2>
+                <h2>{currentTime}</h2>
             </div>
-
             {orders && orders.length > 0 ? (
                 <ul className={styles.produitsDisponibles}>
                     {orders.map((itemsArray, index) => (
                         <React.Fragment key={index}>
-                            {itemsArray.map((item) => (
-                                <li key={item._id} className={styles.produitDisponible}>
-                                    <Image
-                                        className={`${styles.imgCard} ${styles.img}`}
-                                        src={item.src}
-                                        alt={item.alt || 'Default Image'}
-                                        width={Number(item.averageWidth) || 100}
-                                        height={Number(item.averageHeight) || 100}
-                                        onClick={() => router.push(`/produit/${item.name}`)}
-                                    />
-                                    <div>
-                                        <div className={styles.cartFormWragper}>
-                                            <p className={styles.productInfo}>{item.name}</p>
-                                            <p className={styles.productInfo}>Prix: ${item.price}</p>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
+                            {itemsArray.map((item) => {
+                                if (item.purchaseQuantity > 0) {
+                                    return (
+                                        <li key={item._id} className={styles.produitDisponible}>
+                                            <Image
+                                                className={`${styles.imgCard} ${styles.img}`}
+                                                src={item.src}
+                                                alt={item.alt || 'Default Image'}
+                                                width={Number(item.averageWidth) || 100}
+                                                height={Number(item.averageHeight) || 100}
+                                                onClick={() => router.push(`/produit/${item.name}`)}
+                                            />
+                                            <div>
+                                                <div className={styles.cartFormWragper}>
+                                                    <p className={styles.productInfo}>{item.name}</p>
+                                                    <p className={styles.productInfo}>Prix: ${item.price}</p>
+                                                    <p className={styles.productInfo}>Quantite: {item.purchaseQuantity}</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    );
+                                } else {
+                                    return null;
+                                }
+                            })}
                         </React.Fragment>
                     ))}
                 </ul>
