0752652b86ebbfdc69c524796e96227ca9c8f7fe
MiguelJerome <sharkmaycry@gmail.com>
2023-02-19 19:22:00 -0500

    synchronizer cart et commandes

--- components/AchatPanier/PanierVideMessage.jsx
+++ components/AchatPanier/PanierVideMessage.jsx
@@ -1,10 +1,23 @@
 import styles from '/styles/Cart.module.css';
+import Image from 'next/image';
+import Spinner from '/public/img/spinner.gif'
 
-export default function PanierVideMessage() {
+export default function PanierVideMessage({ time }) {
     return (
+        <>
         <h3 className={styles.emptyCartMessage}>
             <span className={styles.shocked} role="img" aria-label="shocked">😱</span>
-            <p>Vous n'avez encore rien ajouté à votre panier!</p>
-        </h3>
+                <p>Vous n'avez encore rien ajouté à votre panier!</p>
+                <p>Votre panier est vide.</p>
+                <p>La page se rechargera dans {time / 1000} secondes.</p>
+                
+            </h3>
+            <Image
+                    src={Spinner}
+                    className={styles.imgSpinner}
+                    alt={'Attente commande mise a jour' || 'Default Image'}
+                    priority={true}
+                />
+        </>
     )
 };
--- components/MagasinCalcul/TotalAchatQuantite.jsx
+++ components/MagasinCalcul/TotalAchatQuantite.jsx
@@ -7,3 +7,4 @@ export default function TotalAchatQuantite({ cart }) {
   });
   return <>{sum.toFixed(0)}</>;
 }
+
