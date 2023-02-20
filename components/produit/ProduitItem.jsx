06a7f9933d0267fc6a0604e59d997dc4cb221a4e
MiguelJerome <sharkmaycry@gmail.com>
2023-02-20 05:56:38 -0500

    break down produit card in multiple components

--- components/produit/ProduitItem.jsx
+++ components/produit/ProduitItem.jsx
@@ -1,55 +1,20 @@
-import Image from 'next/image';
 import styles from '/styles/ProduitCard.module.css';
-import AjouterEnleverPanier from './AjouterEnleverPanier';
+import ProduitInfo from '/components/produit/ProduitItemInfo.jsx';
+import ProduitItemImage from '/components/produit/ProduitItemImage.jsx';
 import React from 'react';
 
 export default function ProduitItem({ product, averageWidth, averageHeight, router, addToCart, handleAddToCart, handleQuantityChange, clearDepart, quantite }) {
-  const { _id, src, alt, name, price, stock } = product;
-
   return (
-    <div key={_id} className={`${styles.container} ${styles.containerItem}`}>
-      <div className={styles.container}>
-        <Image
-          className={styles.imgCard}
-          src={src}
-          alt={alt || 'Default Image'}
-          width={Number(averageWidth) || 400}
-          height={Number(averageHeight) || 400}
-          onClick={() => router.push(`/produit/${name}`)}
-        />
-      </div>
-      <div className={styles.imageInfo}>
-        <p className={styles.imageId}>Produit</p>
-        <p className={styles.imageId}>#{_id}</p>
-        <p className={styles.imageName}>{name}</p>
-        <p className={styles.imagePrice}>C${price}</p>
-        <p className={styles.imageStock}>
-          <span className={styles.stock}>{stock}</span> items en stock
-        </p>
-        <div className={styles.dashBoardButton}>
-          <AjouterEnleverPanier
-            stock={stock}
-            depart={quantite}
-            product={{ _id, name, price }}
-            onQuantityChange={handleQuantityChange}
-            onAddToCart={() => {
-              addToCart({ _id, name, price }, quantite);
-              handleAddToCart({ _id, stock }, quantite, () => handleQuantityChange(0));
-            }}
-            onClearDepart={clearDepart}
-            quantite={quantite}
-          />
-          <button
-            className={styles.button}
-            onClick={() => {
-              handleAddToCart({ _id, stock }, quantite);
-              clearDepart();
-            }}
-          >
-            Ajouter au Panier
-          </button>
-        </div>
-      </div>
+    <div key={product._id} className={`${styles.container} ${styles.containerItem}`}>
+      <ProduitItemImage product={product} averageWidth={averageWidth} averageHeight={averageHeight} router={router} />
+      <ProduitInfo
+        product={product}
+        addToCart={addToCart}
+        handleAddToCart={handleAddToCart}
+        handleQuantityChange={handleQuantityChange}
+        clearDepart={clearDepart}
+        quantite={quantite}
+      />
     </div>
   );
 }
