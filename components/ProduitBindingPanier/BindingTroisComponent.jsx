088ca19216b9f41659662d2c501a97a113550464
MiguelJerome <sharkmaycry@gmail.com>
2023-02-23 17:07:07 -0500

    clean code

--- components/ProduitBindingPanier/BindingTroisComponent.jsx
+++ components/ProduitBindingPanier/BindingTroisComponent.jsx
@@ -8,121 +8,12 @@ import styles from '/styles/AjouterEnleverPanier.module.css';
 
 
 export default function BindingTroisComponent({
-    handleQuantityChange,
-    showPanierPanneau,
-    handleAddToCart,
-    item,
-    onQuantityChange,
-    quantiteProp,
-    decrementer,
-    incrementer
+   
   }) {
-    const [toggler, setToggler] = useState(false);
-    const { name = '', description = '', stock = 0 } = item ?? {};
-    const [, addToCart, cart, setCart] = useCart([]);
-    const [quantite, setQuantite] = useState(quantiteProp);
-  
-    const handleAddToCartClick = () => {
-      handleAddToCart({ _id: item._id, stock }, quantite);
-      clearDepart(stock);
-    };
-  
-    const handleChange = (product, value) => {
-      if (Number.isInteger(value) && Array.isArray(cart)) {
-        const updatedCart = [...cart];
-        const itemIndex = updatedCart.findIndex((i) => i._id === product._id);
-        const stock = updatedCart[itemIndex].stock;
-        const updatedItem = {
-          ...updatedCart[itemIndex],
-          purchaseQuantity: value >= 0 ? Math.min(parseInt(value, 10), stock) : 0,
-        };
-        const newCart = [
-          ...updatedCart.slice(0, itemIndex),
-          updatedItem,
-          ...updatedCart.slice(itemIndex + 1),
-        ];
-        setCart(newCart);
-      }
-    };
-  
-    const clearDepart = (newDepart) => {
-      const updatedProduct = { ...item, stock: newDepart };
-      setCart((prevCart) => {
-        const cartWithoutItem = prevCart.filter((p) => p._id !== item._id);
-        if (newDepart > 0) {
-          return [...cartWithoutItem, updatedProduct];
-        } else {
-          return cartWithoutItem;
-        }
-      });
-      setQuantite(0);
-      setQuantite(newDepart);
-    };
-  
+
     return (
       <>
-        <div>
-          <PanierPanneau toggler={toggler} />
-          <ProduitInfoPanier />
-          <div className={styles.buttonGroup}>
-            <button className={styles.button} onClick={decrementer}>
-              -
-            </button>
-            <div className={styles.panierItemQuantite}>{quantite}</div>
-            <button className={styles.button} onClick={incrementer}>
-              +
-            </button>
-          </div>
-          <ProduitItemBtnAjouterPanier
-            showPanierPanneau={showPanierPanneau}
-            toggler={toggler}
-            handleAddToCartClick={handleAddToCartClick}
-            quantite={quantite}
-          />
-          <button className={styles.buttonClear} onClick={() => clearDepart(0)}>Clear</button>
-        </div>
-  
-        <div className={`${styles.container} ${styles.containerInfo}`}>
-          <h5>Input panier</h5>
-          <h2 className={styles.title}>{name}</h2>
-          <p className={styles.description}>{description}</p>
-          <ProduitInfoPanier />
-          <div className={styles.form}>
-            <div className={styles.formGroup}>
-              <label className={styles.label} htmlFor="quantity">
-                Quantité
-              </label>
-        <div className={styles.inputGroup}>
-          <button className={styles.btn} onClick={decrementer}>-</button>
-          <input
-            className={styles.input}
-            type="number"
-            id="quantity"
-            name="quantity"
-            value={quantite}
-            onChange={(e) => handleQuantityChange(parseInt(e.target.value))}
-          />
-          <button className={styles.btn} onClick={incrementer}>+</button>
-        </div>
-      </div>
-      <button className={styles.btnAdd} onClick={handleAddToCartClick}>
-        Ajouter au panier
-      </button>
-    </div>
-    <ProduitItemDashBoardBouton
-                  stock={stock}
-                  depart={item?.depart}
-                  product={item}
-                  addToCart={addToCart}
-                  handleAddToCart={handleAddToCart}
-                  handleQuantityChange={handleQuantityChange}
-                  clearDepart={clearDepart}
-                  onQuantityChange={onQuantityChange}
-                  quantite={quantite}
-                  decrementer={decrementer}
-                  incrementer={incrementer}
-    />
-  </div>
+
 </>
     );
 }  
--- pages/Binding.jsx
+++ pages/Binding.jsx
@@ -17,36 +17,14 @@ export default function BindingComponentComboPanierProduit(props) {
   const toggler = () => {
     setVisible(!visibleState);
     };
-    const [produitsState, setProduits] = useState(produits);
-    const [, addToCart] = useCart();
-  
-    
-    const handleAddToCart = ({ _id, stock }, quantity) => {
-      if (quantity > stock) {
-        return;
-      }
-      const productIndex = produitsState.findIndex((p) => p._id === _id);
-      const updatedProduct = { ...produitsState[productIndex], stock: stock - quantity };
-      const updatedProduits = [
-        ...produitsState.slice(0, productIndex),
-        updatedProduct,
-        ...produitsState.slice(productIndex + 1),
-      ];
-      setProduits(updatedProduits);
-    };
+
 
   return (
     <>
           <Header />
           {!visibleState && <PanierPanneau toggler={toggler} />}
           <main>
-              <BindingTroisComponent
-            showPanierPanneau={visibleState}
-            item={produits}
-            handleAddToCart={handleAddToCart}
-            handleQuantityChange={ParentComponentBinding.handleQuantityChange} // pass the function as a prop
-            onQuantityChange={() => {}} 
-            />
+         
       </main>
       <Footer />
     </>
